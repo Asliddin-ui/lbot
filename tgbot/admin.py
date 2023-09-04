653ada7c8eed1ccc3155e1b2eb5c1b7d11be8ae5
Asliddin <asliddinjaloliddinov12@gmail.com>
2023-09-04 10:43:33 +0500

    all models created

--- tgbot/admin.py
+++ tgbot/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(RoleModel)
+class RoleModelAdmin(admin.ModelAdmin):
+    list_display = ['id','name']
+
+
+@admin.register(RegionModel)
+class RegionModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+@admin.register(CategoryModel)
+class CategoryModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+@admin.register(TelegramUser)
+class TelegramUserAdmin(admin.ModelAdmin):
+    list_display = ['telegram_user_id', 'fullname', 'phone','category']
--- tgbot/models.py
+++ tgbot/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+from tgbot.decorators import i18n
+
+
+@i18n
+class RegionModel(models.Model):
+    name_uz = models.CharField(max_length=255, default=None)
+    name_ru = models.CharField(max_length=255, default=None)
+    name_en = models.CharField(max_length=255, default=None)
+
+
+@i18n
+class RoleModel(models.Model):
+    name_uz = models.CharField(max_length=255, default=None)
+    name_ru = models.CharField(max_length=255, default=None)
+    name_en = models.CharField(max_length=255, default=None)
+
+
+@i18n
+class CategoryModel(models.Model):
+    name_uz = models.CharField(max_length=255, default=None, blank=True)
+    name_ru = models.CharField(max_length=255, default=None, blank=True)
+    name_en = models.CharField(max_length=255, default=None, blank=True)
+
+
+class TelegramUser(models.Model):
+    telegram_user_id = models.BigIntegerField(primary_key=True)
+    username = models.CharField(max_length=255, default=None, blank=True, null=True)
+    fullname = models.CharField(max_length=255, default=None, blank=True, null=True)
+    phone = models.CharField(max_length=12, default=None, unique=True)
+    language = models.CharField(max_length=2, default='uz')
+    rating_count = models.IntegerField(default=0)
+    rating_stars = models.IntegerField(default=0)
+    category = models.ForeignKey(CategoryModel, on_delete=models.RESTRICT)
+    region = models.ForeignKey(RegionModel, on_delete=models.RESTRICT)
+    role = models.ForeignKey(RoleModel, on_delete=models.RESTRICT)
